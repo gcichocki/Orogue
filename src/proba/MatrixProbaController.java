d6932ae35829f5207e2228011afb3a1bb6738168
Toon <guilhem.cichocki@mail.com>
2017-04-28 12:03:45 +0200

    On acommencé le conytroller !

--- src/proba/MatrixProbaController.java
+++ src/proba/MatrixProbaController.java
@@ -1,7 +1,45 @@
 package proba;
 
+import java.util.ArrayList;
+
 /**
  * Created by toon on 21/04/17.
  */
 public class MatrixProbaController {
+
+    private MatrixProba matrix;
+
+    public MatrixProbaController(int sizeX, int sizeY) {
+        this.matrix = new MatrixProba(sizeX, sizeY);
+    }
+
+    public void playerSpotted(int x, int y){
+        /**
+         * an unit has spotted the player
+         * Reset Map
+         * Set 9 on spotted Tile
+         * Smoothing
+         * Yummy
+         */
+
+
+    }
+
+    public void updateProba(){
+        /**
+         * update the map proba of the unit as the new turn beginsssz
+         */
+    }
+
+
+    public void blendProba(ArrayList<Proba> listProba){
+        /**
+         * Add those probas to the unit mapProba
+         * Smooth the result
+         */
+        this.matrix.setListMaxProba(listProba);
+        this.matrix.smoothMapProba();
+    }
+
+
 }
